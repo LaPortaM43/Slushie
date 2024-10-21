ed3ad631b63d9eab2a0de52a477e2a2c093b536a
Matthew LaPorta <matthewlaporta43@gmail.com>
2024-10-21 11:54:37 -0400

    Update makeDB.sql
    Changed the variable types for the ID's to make it more readable for when we go to populate the database.

--- Database/makeDB.sql
+++ Database/makeDB.sql
@@ -6,7 +6,7 @@ USE SlushieApp_DB;
 
 DROP TABLE IF EXISTS customers; 
 CREATE TABLE customers ( 
-    customerID INT AUTO_INCREMENT PRIMARY KEY, 
+    customerID VARCHAR(50) PRIMARY KEY, 
     name VARCHAR(100) NOT NULL, 
     email VARCHAR(100) UNIQUE NOT NULL,
     customerAddress VARCHAR(255) NOT NULL, 
@@ -16,18 +16,18 @@ TRUNCATE TABLE customers;
 
 DROP TABLE IF EXISTS flavors; 
 CREATE TABLE flavors ( 
-    flavorID INT AUTO_INCREMENT PRIMARY KEY, 
+    flavorID VARCHAR(50) PRIMARY KEY, 
     flavorName VARCHAR(100) UNIQUE NOT NULL
 );
 TRUNCATE TABLE flavors; 
 
 DROP TABLE IF EXISTS favorites;
 CREATE TABLE favorites ( 
-    favoriteID INT AUTO_INCREMENT PRIMARY KEY, 
-    customerID INT NOT NULL, 
-    flavor1ID INT NOT NULL, 
-    flavor2ID INT DEFAULT NULL,
-    flavor3ID INT DEFAULT NULL,
+    favoriteID VARCHAR(50) PRIMARY KEY, 
+    customerID VARCHAR(50) NOT NULL, 
+    flavor1ID VARCHAR(50) NOT NULL, 
+    flavor2ID VARCHAR(50) DEFAULT NULL,
+    flavor3ID VARCHAR(50) DEFAULT NULL,
     FOREIGN KEY (customerID) REFERENCES customers(customerID),
     FOREIGN KEY (flavor1ID) REFERENCES flavors(flavorID),
     FOREIGN KEY (flavor2ID) REFERENCES flavors(flavorID),
@@ -35,26 +35,26 @@ CREATE TABLE favorites (
 );
 TRUNCATE TABLE favorites; 
 
-DROP TABLE IF EXISTS restaurant;
-CREATE TABLE restaurant ( 
-    restaurantID INT AUTO_INCREMENT PRIMARY KEY, 
+DROP TABLE IF EXISTS restaurants;
+CREATE TABLE restaurants ( 
+    restaurantID VARCHAR(50) PRIMARY KEY, 
     restaurantName VARCHAR(100) NOT NULL, 
     restaurantAddress VARCHAR(255) UNIQUE NOT NULL
 );
-TRUNCATE TABLE restaurant; 
+TRUNCATE TABLE restaurants; 
 
 DROP TABLE IF EXISTS orders; 
 CREATE TABLE orders ( 
-    orderID INT AUTO_INCREMENT PRIMARY KEY, 
-    customerID INT NOT NULL,
-    orderPrice INT NOT NULL,  
+    orderID VARCHAR(50) PRIMARY KEY, 
+    customerID VARCHAR(50) NOT NULL,
+    orderPrice VARCHAR(50) NOT NULL,  
     deliveryAddress VARCHAR(255) DEFAULT NULL,  
-    restaurantID INT NOT NULL, 
-    flavor1ID INT NOT NULL,  
-    flavor2ID INT DEFAULT NULL,  
-    flavor3ID INT DEFAULT NULL,  
+    restaurantID VARCHAR(50) NOT NULL, 
+    flavor1ID VARCHAR(50) NOT NULL,  
+    flavor2ID VARCHAR(50) DEFAULT NULL,  
+    flavor3ID VARCHAR(50) DEFAULT NULL,  
     FOREIGN KEY (customerID) REFERENCES customers(customerID),
-    FOREIGN KEY (restaurantID) REFERENCES restaurant(restaurantID),
+    FOREIGN KEY (restaurantID) REFERENCES restaurants (restaurantID),
     FOREIGN KEY (flavor1ID) REFERENCES flavors(flavorID),
     FOREIGN KEY (flavor2ID) REFERENCES flavors(flavorID),
     FOREIGN KEY (flavor3ID) REFERENCES flavors(flavorID)
