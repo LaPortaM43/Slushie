1f71aacc2d849649e9c42c7341b1de2b1c245e3a
Matthew LaPorta <matthewlaporta43@gmail.com>
2024-10-24 14:50:44 -0400

    Update db.js

--- db.js
+++ db.js
@@ -1,21 +1,20 @@
 // db.js
-const mysql = require('mysql2');
+
+// You're going to need to install 'dotenv' by doing "npm install dotenv", and make a new file called ".env" to store variables 
+// Also will have to change type in "package.json" to "modules" to allow for importing and exporting 
+import mysql from 'mysql2';
+import dotenv from 'dotenv';
+dotenv.config(); 
 
 // Create a connection to the database
-const connection = mysql.createConnection({
-  host: 'localhost',  
-  user: 'root',  
-  password: 'Slushie123', 
-  database: 'SlushieApp_DB' 
-});
+// dotenv, process.env.EXAMPLE lets you store variables in another file called ".env" 
+const pool = mysql.createPool({
+  host: process.env.MYSQL_HOST,
+  user: process.env.MYSQL_USER,
+  password: process.env.MYSQL_PASSWORD, 
+  database: process.env.MYSQL_DATABASE 
+}).promise(); // Promise is an object representing the eventual completion of an asynchronous operation 
+// Async function always returns a "promise", and "await" pauses execution of the function until the "promise" is resolved 
 
-// Connect to the database
-connection.connect((err) => {
-  if (err) {
-    console.error('Error connecting to the database: ', err.stack);
-    return;
-  }
-  console.log('Connected to the database.'); 
-});
-
-module.exports = connection; // Export the connection for use in other files
+// Make the connection available to other files 
+export {pool};
