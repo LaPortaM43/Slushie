961601bfdc116f6e715589dfe195e4f436392370
Matthew LaPorta <matthewlaporta43@gmail.com>
2024-10-28 13:21:43 -0400

    Update populateDB.sql

--- Database/populateDB.sql
+++ Database/populateDB.sql
@@ -6,19 +6,19 @@ INSERT INTO customers VALUES ('c3', 'Matt', 'matt@gmail.com', '400 Cedar Ave, We
 INSERT INTO customers VALUES ('c4', 'Max', 'max@gmail.com', '400 Cedar Ave, West Long Branch, NJ 07764', 'max123'); 
 INSERT INTO customers VALUES ('c5', 'Tom', 'tom@gmail.com', '400 Cedar Ave, West Long Branch, NJ 07764', 'tom123'); 
  
-INSERT INTO flavors VALUES ('flv1', 'Vanilla');
-INSERT INTO flavors VALUES ('flv2', 'Chocolate');
-INSERT INTO flavors VALUES ('flv3', 'Strawberry');
-INSERT INTO flavors VALUES ('flv4', 'Blueberry');
-INSERT INTO flavors VALUES ('flv5', 'Blue Raspberry');
+INSERT INTO flavors VALUES ('flav1', 'Vanilla');
+INSERT INTO flavors VALUES ('flav2', 'Chocolate');
+INSERT INTO flavors VALUES ('flav3', 'Strawberry');
+INSERT INTO flavors VALUES ('flav4', 'Blueberry');
+INSERT INTO flavors VALUES ('flav5', 'Blue Raspberry');
 
-INSERT INTO favorites VALUES ('f1', 'c1', 'flv1', 'flv4', NULL); 
-INSERT INTO favorites VALUES ('f2', 'c2', 'flv5', NULL, NULL); 
-INSERT INTO favorites VALUES('f3', 'c3', 'flv1', 'flv3', NULL); 
-INSERT INTO favorites VALUES('f4', 'c4', 'flv2', NULL, NULL); 
-INSERT INTO favorites VALUES('f5', 'c5', 'flv5', NULL, NULL);
+INSERT INTO combo VALUES ('cb1', 'c1', 'flav1', 'flav4', NULL); 
+INSERT INTO combo VALUES ('cb2', 'c2', 'flav5', NULL, NULL); 
+INSERT INTO combo VALUES('cb3', 'c3', 'flav1', 'flav3', NULL); 
+INSERT INTO combo VALUES('cb4', 'c4', 'flav2', NULL, NULL); 
+INSERT INTO combo VALUES('cb5', 'c5', 'flav5', NULL, NULL);
 
-INSERT INTO restaurants VALUES ('r1', 'slushie', '400 Cedar Ave, West Long Branch, NJ 07764'); 
+INSERT INTO branches VALUES ('b1', 'slushie', '400 Cedar Ave, West Long Branch, NJ 07764'); 
 
-INSERT INTO orders VALUES('o1', 'c1', 10, NULL, 'r1', 'flv1', 'flv3', NULL);
+INSERT INTO orders VALUES('o1', 'c1', 10, NULL, 'b1', 'flav1', 'flav3', NULL);
 
