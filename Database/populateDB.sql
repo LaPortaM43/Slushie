2549a925dd656776dc607170414dff9d8ddf89ea
Matthew LaPorta <matthewlaporta43@gmail.com>
2024-11-01 12:22:44 -0400

    Update populateDB.sql

--- Database/populateDB.sql
+++ Database/populateDB.sql
@@ -3,7 +3,7 @@
 INSERT INTO customers VALUES ('c1', 'Bob', 'Bob@gmail.com', '400 Cedar Ave, West Long Branch, NJ 07764', 'bob123'); 
 INSERT INTO customers VALUES ('c2', 'Bill', 'Bill@gmail.com', '400 Cedar Ave, West Long Branch, NJ 07764', 'bill123'); 
 INSERT INTO customers VALUES ('c3', 'Matt', 'matt@gmail.com', '400 Cedar Ave, West Long Branch, NJ 07764', 'matt123'); 
-INSERT INTO customers VALUES ('c4', 'Max', 'max@gmail.com', '400 Cedar Ave, West Long Branch, NJ 07764', 'max123'); 
+INSERT INTO customers VALUES ('c4', 'Mark', 'mark@gmail.com', '400 Cedar Ave, West Long Branch, NJ 07764', 'max123'); 
 INSERT INTO customers VALUES ('c5', 'Tom', 'tom@gmail.com', '400 Cedar Ave, West Long Branch, NJ 07764', 'tom123'); 
  
 INSERT INTO flavors VALUES ('flav1', 'Vanilla');
@@ -19,6 +19,7 @@ INSERT INTO combo VALUES('cb4', 'c4', 'flav2', NULL, NULL);
 INSERT INTO combo VALUES('cb5', 'c5', 'flav5', NULL, NULL);
 
 INSERT INTO branches VALUES ('b1', 'slushie', '400 Cedar Ave, West Long Branch, NJ 07764'); 
+INSERT INTO branches VALUES ('b2', 'slushie', '500 Cedar Ave, West Long Branch, NJ 07764'); 
 
 INSERT INTO orders VALUES('o1', 'c1', 10, NULL, 'b1', 'flav1', 'flav3', NULL);
 
