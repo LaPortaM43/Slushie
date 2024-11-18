845aab700cb484ac30d4a36a667788b91ba221f6
Matthew LaPorta <matthewlaporta43@gmail.com>
2024-11-18 11:17:24 -0500

    Update flavor.js

--- models/flavor.js
+++ models/flavor.js
@@ -3,7 +3,10 @@
 const { DataTypes } = require('sequelize');
 const db = require('../config/database');
 
-const Flavor = db.define('Flavor', {
+const Order = require('./order'); 
+const Combo = require('./combo');
+class Flavor extends model{} 
+Flavor.init({
     flavorID: { 
         type: DataTypes.STRING(100), 
         primaryKey: true, 
@@ -14,6 +17,18 @@ const Flavor = db.define('Flavor', {
         allowNull: false, 
         unique: true 
     }
-});
+}, { 
+    sequelize: db, 
+    modelName: 'Flavor',
+
+})
+
+Flavor.hasMany(Order, { foreignKey: 'flavor1ID', as: 'flavor1Orders' });
+Flavor.hasMany(Order, { foreignKey: 'flavor2ID', as: 'flavor2Orders' });
+Flavor.hasMany(Order, { foreignKey: 'flavor3ID', as: 'flavor3Orders' });
+
+Flavor.hasMany(Combo, { foreignKey: 'flavor1ID', as: 'flavor1Combos' });
+Flavor.hasMany(Combo, { foreignKey: 'flavor2ID', as: 'flavor2Combos' });
+Flavor.hasMany(Combo, { foreignKey: 'flavor3ID', as: 'flavor3Combos' });
 
 module.exports = Flavor;
