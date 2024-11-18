90f25c0311257d367bec05ab946dc2951b0d409e
ryanwire123 <59776091+ryanwire123@users.noreply.github.com>
2024-11-18 11:20:43 -0500

    Create flavors.js

---
+++ routes/flavors.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const router = express.Router();
+const authenticateToken = require('../middlewares/auth');
+const { getAllFlavors, getFlavorById, createFlavor, updateFlavor, deleteFlavor } = require('../controllers/flavors');
+
+router.use(authenticateToken);
+
+// Flavor Routes
+router.get('/', getAllFlavors);
+router.get('/:id', getFlavorById);
+router.post('/', createFlavor);
+router.put('/:id', updateFlavor);
+router.delete('/:id', deleteFlavor);
+
+module.exports = router;
