700ea2ddc6c3d8559ae20b7ab65aaf3f4ecf76cf
ryanwire123 <59776091+ryanwire123@users.noreply.github.com>
2024-11-18 11:18:58 -0500

    Create combos.js

---
+++ routes/combos.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const router = express.Router();
+const authenticateToken = require('../middlewares/auth');
+const { getAllCombos, getComboById, createCombo, updateCombo, deleteCombo } = require('../controllers/combos');
+
+router.use(authenticateToken);
+
+// Combo Routes
+router.get('/', getAllCombos);
+router.get('/:id', getComboById);
+router.post('/', createCombo);
+router.put('/:id', updateCombo);
+router.delete('/:id', deleteCombo);
+
+module.exports = router;
