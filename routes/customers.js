9528bcafe1953afe7dea855ef60fb7cf4f9ddd18
ryanwire123 <59776091+ryanwire123@users.noreply.github.com>
2024-11-18 11:20:10 -0500

    Create customers.js

---
+++ routes/customers.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const router = express.Router();
+const authenticateToken = require('../middlewares/auth');
+const { getAllCustomers, getCustomerById, createCustomer, updateCustomer, deleteCustomer } = require('../controllers/customers');
+
+router.use(authenticateToken);
+
+// Customer Routes
+router.get('/', getAllCustomers);
+router.get('/:id', getCustomerById);
+router.post('/', createCustomer);
+router.put('/:id', updateCustomer);
+router.delete('/:id', deleteCustomer);
+
+module.exports = router;
