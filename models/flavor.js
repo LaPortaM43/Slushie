12f244602ca2746085b0998fdc3a2e531d7d38f9
ryanwire123 <59776091+ryanwire123@users.noreply.github.com>
2024-11-20 10:11:21 -0500

    Update flavor.js

--- models/flavor.js
+++ models/flavor.js
@@ -6,7 +6,8 @@ const db = require('../config/database.js');
 const Order = require('./order.js'); 
 const Combo = require('./combo.js');
 
-class Flavor extends Model{} 
+class Flavor extends Model{}
+
 Flavor.init({
     flavorID: { 
         type: DataTypes.STRING(100), 
@@ -21,17 +22,17 @@ Flavor.init({
 }, { 
     sequelize: db,  
     modelName: 'Flavor',
+});
 
-})
-
-/*
+// Associations
+// One flavor can appear in many orders in one of three positions
 Flavor.hasMany(Order, { foreignKey: 'flavor1ID', as: 'flavor1Orders' });
 Flavor.hasMany(Order, { foreignKey: 'flavor2ID', as: 'flavor2Orders' });
 Flavor.hasMany(Order, { foreignKey: 'flavor3ID', as: 'flavor3Orders' });
 
+// One flavor can appear in many combos in one of three positions
 Flavor.hasMany(Combo, { foreignKey: 'flavor1ID', as: 'flavor1Combos' });
 Flavor.hasMany(Combo, { foreignKey: 'flavor2ID', as: 'flavor2Combos' });
 Flavor.hasMany(Combo, { foreignKey: 'flavor3ID', as: 'flavor3Combos' });
-*/
-module.exports = Flavor;
 
+module.exports = Flavor;
