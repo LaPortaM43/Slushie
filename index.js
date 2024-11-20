430ce96ea150bcc50f44af577ad50fee65cd5af1
Matthew LaPorta <matthewlaporta43@gmail.com>
2024-11-20 10:14:53 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,65 +1,64 @@
 // index.js
+
 const express = require('express');
 const bodyParser = require('body-parser');
-const db = require('./config/database'); // Database configuration
+const db = require('./config/database.js'); // Database configuration
 const dotenv = require('dotenv');
 const cors = require('cors');
 dotenv.config();
 
 // Import routes
-const authRoutes = require('./routes/auth');
-const customerRoutes = require('./routes/customers');
-const flavorRoutes = require('./routes/flavors');
-const comboRoutes = require('./routes/combos');
-const branchRoutes = require('./routes/branches');
-const orderRoutes = require('./routes/orders');
+const authRoutes = require('./routes/auth.js');
+const customerRoutes = require('./routes/customers.js');
+const flavorRoutes = require('./routes/flavors.js');
+const comboRoutes = require('./routes/combos.js');
+const branchRoutes = require('./routes/branches.js');
+const orderRoutes = require('./routes/orders.js');
 
 
 //import models
-const Customer = require('./models/customer');
-const Order = require('./models/order');
-const Flavor = require('./models/flavor');
-const Branch = require('./models/branch');
-const Combo = require('./models/combo');
+const Customer = require('./models/customer.js');
+const Order = require('./models/order.js');
+const Flavor = require('./models/flavor.js');
+const Branch = require('./models/branch.js');
+const Combo = require('./models/combo.js');
 
 // Initialize Express
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+
 // Middleware
 app.use(bodyParser.json());
 app.use(express.urlencoded({ extended: true }));
-/*app.use(cors({
+app.use(cors({
     origin: 'https://laportam43.github.io/Slushie', 
     methods: 'GET,POST,PUT,DELETE',
     credentials: true,
-}));*/
+}));
+
+
 
 //Define Associations
+/*
 Customer.hasMany(Order, { foreignKey: 'customerID', as: 'orders' });
 Order.belongsTo(Customer, { foreignKey: 'customerID', as: 'customer' });
+
 
 Flavor.hasMany(Order, { foreignKey: 'flavor1ID', as: 'flavor1Orders' });
 Flavor.hasMany(Order, { foreignKey: 'flavor2ID', as: 'flavor2Orders' });
 Flavor.hasMany(Order, { foreignKey: 'flavor3ID', as: 'flavor3Orders' });
 
-Order.belongsTo(Flavor, { foreignKey: 'flavor1ID', as: 'Flavor1' });
-Order.belongsTo(Flavor, { foreignKey: 'flavor2ID', as: 'Flavor2' });
-Order.belongsTo(Flavor, { foreignKey: 'flavor3ID', as: 'Flavor3' });
-
-Branch.hasMany(Order, { foreignKey: 'branchID', as: 'branchOrders' });
-Order.belongsTo(Branch, { foreignKey: 'branchID', as: 'branch' });
-
-Customer.hasMany(Combo, { foreignKey: 'customerID', as: 'combos' });
-Combo.belongsTo(Customer, { foreignKey: 'customerID', as: 'customer' });
-
 Flavor.hasMany(Combo, { foreignKey: 'flavor1ID', as: 'flavor1Combos' });
 Flavor.hasMany(Combo, { foreignKey: 'flavor2ID', as: 'flavor2Combos' });
 Flavor.hasMany(Combo, { foreignKey: 'flavor3ID', as: 'flavor3Combos' });
 
-Combo.belongsTo(Flavor, { foreignKey: 'flavor1ID', as: 'Flavor1' });
-Combo.belongsTo(Flavor, { foreignKey: 'flavor2ID', as: 'Flavor2' });
-Combo.belongsTo(Flavor, { foreignKey: 'flavor3ID', as: 'Flavor3' });
+Order.belongsTo(Customer, { foreignKey: 'customerID' });
+Order.belongsTo(Branch, { foreignKey: 'branchID' });
+Order.belongsTo(Flavor, { foreignKey: 'flavor1ID', as: 'Flavor1' });
+Order.belongsTo(Flavor, { foreignKey: 'flavor2ID', as: 'Flavor2' });
+Order.belongsTo(Flavor, { foreignKey: 'flavor3ID', as: 'Flavor3' });
+*/
 
 // Routes
 app.use('/api/auth', authRoutes);
