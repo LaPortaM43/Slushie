4c3e3582a2bd517bb664e37615fccd6cc0139180
Matthew LaPorta <matthewlaporta43@gmail.com>
2024-11-21 19:58:15 -0500

    Add files via upload

---
+++ middleware/authenticateToken.js
@@ -0,0 +1,18 @@
+// middleware/authenticateToken.js
+
+import jwt from 'jsonwebtoken'; 
+import dotenv from 'dotenv'; 
+dotenv.config();
+
+const authenticateToken = (req, res, next) => {
+  const token = req.headers['authorization']?.split(' ')[1];
+  if (!token) return res.sendStatus(401);
+
+  jwt.verify(token, process.env.JWT_SECRET, (err, user) => {
+    if (err) return res.sendStatus(403);
+    req.user = user;
+    next();
+  });
+};
+
+export { authenticateToken }; 
